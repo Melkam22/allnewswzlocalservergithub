a9234356724a45baf74017d894aa2fa8cff3b063
Ashu <ashenshi@gmail.com>
2020-08-04 17:40:52 +0200

    local server created and data added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ class App extends Component{
           return(
             <div key={a}>
               <p>{item.id}</p>
+              <p>{item.name}</p>
+              <p>{item.language}</p>
                  <a href={item.url} target="_blank"><img src={`/Images/${item.givenImage}`} alt='mesel' className="mesel"/></a>
                 <p><a href={item.url} target="_blank">{item.url}</a></p>
             </div>
